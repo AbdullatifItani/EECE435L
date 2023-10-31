0e0eee0e0d44d107fc472fba5e0ab29e315c3b6b
Abdullatif Itani <awi03@mail.aub.edu>
2023-10-31 18:18:27 +0200

    API Tests Using Postman Push

--- database.py
+++ database.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python 
 import sqlite3
+from flask import Flask, request, jsonify #added to top of file 
+from flask_cors import CORS #added to top of file
+
 def connect_to_db():
     conn = sqlite3.connect('database.db') 
     return conn
@@ -38,7 +41,8 @@ def insert_user(user):
 
 user = {
     "name": "John Doe",
-    "email": "jondoe@gamil.com", "phone": "067765434567",
+    "email": "jondoe@gamil.com", 
+    "phone": "067765434567",
     "address": "John Doe Street, Innsbruck",
     "country": "Austria" 
 }
@@ -114,9 +118,43 @@ def delete_user(user_id):
         conn.close() 
     return message
 
-#create_db_table()
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+@app.route('/api/users', methods=['GET']) 
+def api_get_users():
+    return jsonify(get_users())
+
+@app.route('/api/users/<user_id>', methods=['GET'])
+def api_get_user(user_id):
+    return jsonify(get_user_by_id(user_id))
+
+@app.route('/api/users/add', methods = ['POST']) 
+def api_add_user():
+    user = request.get_json()
+    return jsonify(insert_user(user))
+
+@app.route('/api/users/update', methods = ['PUT']) 
+def api_update_user():
+    user = request.get_json()
+    return jsonify(update_user(user))
+@app.route('/api/users/delete/<user_id>', methods = ['DELETE']) 
+def api_delete_user(user_id):
+    return jsonify(delete_user(user_id))
+
+if __name__ == "__main__": 
+    app.debug = True 
+    app.run(debug=True) 
+    app.run() #run app
+
+#create_db_table() #Uncomment this if you have not created a table
 #insert_user(user)
 #print(get_users())
 #get_user_by_id(user_id)
 #update_user(user)
 #delete_user(user_id)
+#api_get_users()
+#api_get_user(user_id)
+#api_add_user()
+#api_update_user()
+#api_delete_user(user_id)
